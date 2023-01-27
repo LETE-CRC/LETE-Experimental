a2d252578e15f0115f5804b5af36a7d1b6362941
HelioVillanueva <heliohenriquevillanueva@gmail.com>
2023-01-27 17:12:06 -0300

    reestruturacao em classe/funcoes
    Reestruturacao para processar lista de casos

--- SCRE/SCRE-Chemluminescence.py
+++ SCRE/SCRE-Chemluminescence.py
@@ -9,6 +9,7 @@
 ===============================================================================
 version:0.0 - 04/2022: Helio Villanueva
 version:0.1 - 05/2022: Helio Villanueva
+version:0.2 - 01/2023: Helio Villanueva
 """
 
 import numpy as np
@@ -22,117 +23,141 @@ import os
 # -- USER
 # ******************************************************************************
 
-path = "DF01-NL"
-imFmt = 'tif'
-rCAD = 1  # ratio CAD/Time step (1, 2...)
-init0 = 0  # number of initial images discarted (eg: 360)
-saveImgs = True  # save mean and stdDev images for each CAD
+paths = ["DF01-NL"]
 
-# ******************************************************************************
-# -- MAIN
-# ******************************************************************************
+inputs = {'imFmt':'tif',
+          'rMotor':2000,  # rpm rotacao do motor
+          'fCAM':24000,  # Hz taxa aquisicao cameras
+          'init0':0,  # number of initial images discarted (eg 360)
+          'durationCADs':150,  # imgs with combustion to save
+          'saveImgs':True  # save mean and stdDev images for each CAD
+          }
 
-header = '\n' + 70*"=" + '\n' + '\t\tPython code for SCRE analysis\n'
-header += 'Created by Combustion Research Center CRC at LETE - Sao Paulo, Brasil\n'
-header += 'Laboratory of Environmental and Thermal Engineering - LETE\n'
-header += 'Escola Politecnica da USP - EPUSP\n'
-header += 70*"=" + '\n'
-print(header)
+# *****************************************************************************
+# -- FUNCOES
+# *****************************************************************************
 
-imgNames = glob.glob(path + '/*[0-9].' + imFmt)  # List images in Dir 'path'
-imgNames.sort()
-imgNames = imgNames[init0:]  # discard initial images
 
-img = image.imread(imgNames[0])  # read single img for general infos
-lins = img.shape[0]  # y coord
-cols = img.shape[1]  # x coord
-stepsOrig = len(imgNames)  # tot of all imgs saved by the camera
-CADs = int(719 / rCAD)  # ratio steps / cycle
-cycles = int(stepsOrig / CADs)  # total steps / cycle
+class Caso():
+    '''Classe que organiza o caso
+    '''
 
-steps = cycles * CADs
-imgNames = imgNames[:steps]
-limgNames = np.array(imgNames).reshape(cycles, CADs)
+    def __init__(self,path, inputs):
+        self.path = path
+        self.inputs = inputs
+        self.lins, self.cols, self.CADs, self.cycles, self.limgNames, self.infos = self.baseInfos()
 
-print('General Infos')
-print(14*'-', '\nImage res: ', lins, 'x', cols)
-print('CADs/cycle: ', CADs)
-print('Cycles: ', cycles)
-print(14*'-')
+    def baseInfos(self):
+        '''Basic informations
+        '''
+        # List images in Dir 'path'
+        imgNames = glob.glob(self.path + '/*[0-9].' + self.inputs['imFmt'])
+        imgNames.sort()
+        imgNames = imgNames[self.inputs['init0']:]  # discard initial images
 
-# -- Background image for removal process
-# ******************************************************************************
-imsCy = np.zeros((lins, cols, cycles))
-for cy in range(cycles):  # loop over cycles
-    #print("Cycle No: ", cy)
-    imsCy[:, :, cy] = image.imread(limgNames[cy, 0])
+        img = image.imread(imgNames[0])  # read single img for general infos
+        lins = img.shape[0]  # y coord
+        cols = img.shape[1]  # x coord
+        stepsOrig = len(imgNames)  # tot of all imgs saved by the camera
+        singleCycle = 2*360  # deg
+        rCAD = (singleCycle*self.inputs['rMotor']/60)/self.inputs['fCAM']  # CAD / image
+        CADs = int(719 / rCAD)  # CADs observed in each cycle
+        cycles = int(stepsOrig / CADs)  # total steps / cycle
 
-imBackground = np.mean(imsCy, 2)
+        steps = cycles * CADs
+        imgNames = imgNames[:steps]
+        limgNames = np.array(imgNames).reshape(cycles, CADs)
 
-# -- For loops for each cycle and CAD
-# ******************************************************************************
-try:
-    print('Trying to read npy files')
-    imsCycleMean = np.load(path + '/imsCycleMean.npy')
-    imsCycleStdDev = np.load(path + '/imsCycleStdDev.npy')
-    print('done')
-except:
-    print('Reading raw img files')
-    imsCycleMean = np.zeros((lins, cols, CADs))
-    imsCycleStdDev = np.zeros((lins, cols, CADs))
+        infos = 'General Infos\n'
+        infos += 14*'-' + '\nImage res: %.1f x %.1f\n' %(lins, cols)
+        infos += 'CADs/image: %.2f\n' %rCAD
+        infos += 'CADs/cycle: %.1f\n' %CADs
+        infos += 'Cycles: %.1f\n' %cycles
+        infos += 14*'-'
 
-    for t in tqdm(range(CADs), desc="CAD calculations: "):
-        imsCy = np.zeros((lins, cols, cycles))
-        for cy in range(cycles):  # loop over cycles
-            imsCy[:, :, cy] = image.imread(limgNames[cy, t]) - imBackground
+        return lins, cols, CADs, cycles, limgNames, infos
 
-        # hole cycle calculation
-        imsCy[imsCy<0] = 0  # No negative values after background removal
-        imM = np.mean(imsCy, 2, keepdims=True)
-        imsCycleMean[:, :, t] = imM[:, :, 0]
-        imsCyFluct = np.sqrt((imsCy[:, :, :] - imM)**2)
-        imsCycleStdDev[:, :, t] = np.mean(imsCyFluct, 2)
-    print('Saving .npy arrays')
-    np.save(path + '/imsCycleMean', imsCycleMean)
-    np.save(path + '/imsCycleStdDev', imsCycleStdDev)
+    def imgBackground(self):
+        '''Background image for removal process
+        '''
+        imsCy = np.zeros((self.lins, self.cols, self.cycles))
+        for cy in range(self.cycles):  # loop over cycles
+            # print("Cycle No: ", cy)
+            imsCy[:, :, cy] = image.imread(self.limgNames[cy, 0])
 
-# ******************************************************************************
+        return np.mean(imsCy, 2)
 
-print('END Calculations')
+    def imgProcess(self):
+        '''For loops for each cycle and CAD
+        '''
+        imgBackground = self.imgBackground()
 
-# ******************************************************************************
-# -- PLOTS
-# ******************************************************************************
-# min max for plots
-vMeanMin = imsCycleMean.min()
-vMeanMax = imsCycleMean.max()
-vStdDevMin = imsCycleStdDev.min()
-vStdDevMax = imsCycleStdDev.max()
+        try:
+            print('Trying to read npy files')
+            imsCycleMean = np.load(self.path + '/imsCycleMean.npy')
+            imsCycleStdDev = np.load(self.path + '/imsCycleStdDev.npy')
+            print('done')
+        except Exception:
+            print('Reading raw img files')
+            imsCycleMean = np.zeros((self.lins, self.cols, self.CADs))
+            imsCycleStdDev = np.zeros((self.lins, self.cols, self.CADs))
 
-print('Saving images')
-if not os.path.exists('./CADmean'):
-    os.makedirs('./CADmean')
+            for t in tqdm(range(self.CADs), desc="CAD calculations: "):
+                if t > self.inputs['durationCADs']:
+                    break
+                imsCy = np.zeros((self.lins, self.cols, self.cycles))
+                for cy in range(self.cycles):  # loop over cycles
+                    imsCy[:, :, cy] = image.imread(self.limgNames[cy, t])
+                    imsCy[:, :, cy] -= imgBackground
 
-if not os.path.exists('./CADstdDev'):
-    os.makedirs('./CADstdDev')
+                # hole cycle calculation
+                imsCy[imsCy<0] = 0  # No negative values after background removal
+                imM = np.mean(imsCy, 2, keepdims=True)
+                imsCycleMean[:, :, t] = imM[:, :, 0]
+                imsCyFluct = np.sqrt((imsCy[:, :, :] - imM)**2)
+                imsCycleStdDev[:, :, t] = np.mean(imsCyFluct, 2)
+            print('Saving .npy arrays')
+            np.save(self.path + '/imsCycleMean', imsCycleMean)
+            np.save(self.path + '/imsCycleStdDev', imsCycleStdDev)
 
-for t in tqdm(range(CADs), desc="Saving CAD imgs: "):
-    # Mean
-    plt.figure()
-    plt.imshow(imsCycleMean[:, :, t], cmap='hot', vmin=vMeanMin, vmax=vMeanMax)
-    plt.title('mean CAD %3d' %t)
-    plt.colorbar()
-    figNameMean = './CADmean/CADmean' + str(t) + '.png'
-    plt.savefig(figNameMean)
-    plt.close()
-    # StdDev
-    plt.figure()
-    plt.imshow(imsCycleStdDev[:, :, t], cmap='hot', vmin=vStdDevMin, vmax=vStdDevMax)
-    plt.title('stdDev CAD %3d' %t)
-    plt.colorbar()
-    figNameStdDev = './CADstdDev/CADstdDev' + str(t) + '.png'
-    plt.savefig(figNameStdDev)
-    plt.close()
+        return imsCycleMean, imsCycleStdDev
+
+    def Plots(self,imsCycleMean,imsCycleStdDev):
+        '''
+        '''
+        # min max for plots
+        vMeanMin = imsCycleMean.min()
+        vMeanMax = imsCycleMean.max()
+        vStdDevMin = imsCycleStdDev.min()
+        vStdDevMax = imsCycleStdDev.max()
+
+        print('Saving images')
+        if not os.path.exists(self.path + '/CADmean'):
+            os.makedirs(self.path + '/CADmean')
+
+        if not os.path.exists(self.path + '/CADstdDev'):
+            os.makedirs(self.path + '/CADstdDev')
+
+        for t in tqdm(range(self.CADs), desc="Saving CAD imgs: "):
+            if t > self.inputs['durationCADs']:
+                break
+            # Mean
+            plt.figure()
+            plt.imshow(imsCycleMean[:, :, t], cmap='hot', vmin=vMeanMin, vmax=vMeanMax)
+            plt.title('mean CAD %3d' %t)
+            plt.colorbar()
+            figNameMean = self.path + '/CADmean/CADmean%04d' %t + '.png'
+            plt.savefig(figNameMean)
+            plt.close()
+            # StdDev
+            plt.figure()
+            plt.imshow(imsCycleStdDev[:, :, t], cmap='hot', vmin=vStdDevMin, vmax=vStdDevMax)
+            plt.title('stdDev CAD %3d' %t)
+            plt.colorbar()
+            figNameStdDev = self.path + '/CADstdDev/CADstdDev%04d' %t + '.png'
+            plt.savefig(figNameStdDev)
+            plt.close()
+        return 0
 
 ################################################################################
 '''
@@ -140,3 +165,30 @@ TODOS:
 - cortar imagens pra ajustar bordas
 - if a prova de numero errado no rCAD
 '''
+
+# *****************************************************************************
+# -- MAIN
+# *****************************************************************************
+
+
+def main():
+    '''Funcao main caso arquivo seja disparado via terminal
+    '''
+    header = '\n' + 70*"=" + '\n' + '\t\tPython code for SCRE analysis\n'
+    header += 'Created by Combustion Research Center CRC at LETE - Sao Paulo, Brasil\n'
+    header += 'Laboratory of Environmental and Thermal Engineering - LETE\n'
+    header += 'Escola Politecnica da USP - EPUSP\n'
+    header += 70*"=" + '\n'
+    print(header)
+
+    for path in paths:
+        print('Processando caso %s' %path)
+        case = Caso(path,inputs)
+        imgMean, imgDev = case.imgProcess()
+        case.Plots(imgMean,imgDev)
+    return 0
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
